75b630b511a4f02d510cde9b58e60568e7d8f907
Salman Ahmad <salman.neim@gmail.com>
2019-03-02 20:06:11 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,57 @@
 def first_challenge
-   
+   epic_tragedy = {
+  :montague => {
+    :patriarch => {
+      :name => "Lord Montague", 
+      :age => "53"
+    },
+    :matriach => {
+      :name => "Lady Montague",
+      :age => "54"  
+    },
+    :hero => {
+      :name => "Romeo",
+      :age => "15",
+    },
+    :hero_friends => [
+      {
+        :name => "Benviolo",
+        :age => "17",
+        :attitude => "worried"
+      },
+      {
+      :name => "Mercutio",
+      :age => "18",
+      :attitude => "hot-headed",
+    },
+    ]
+  },
+  :capulet => {
+    :patriach => {
+      :name => "Lord Capulet",
+      :age => "50"
+    },
+    :matriach => {
+      :name => "Lady Capulet",
+      :age => "51"
+    },
+    :heroine => {
+      :name => "Juliet",
+      :age => "15",
+      :status => "alive"
+    },
+    :heroine_friends => [
+      {
+        :name => "Steven",
+        :age => "30",
+        :attutude => "confused"
+      },
+      {
+        :name => "Nurse",
+        :age => "44",
+        :attitude => "worried"
+      }
+    ]
+  }
+}
 end
